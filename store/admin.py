38a41d5a86019be40a2f388c9a875853434a72b1
Tilak Dave <davetilak003@gmail.com>
2022-06-21 14:25:16 +0530

    update

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Store, StoreSetting, Order
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(StoreSetting)
+admin.site.register(Order)
