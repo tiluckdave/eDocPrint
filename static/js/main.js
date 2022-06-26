55bb064ab84b096835a0cd1cd8dc290f25dc5757
Tilak Dave <davetilak003@gmail.com>
2022-06-26 10:48:32 +0530

    load

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
-function loading(msg) {
-  ele = document.getElementById("login_button");
+function loading(ele, msg) {
   ele.innerHTML = `
   <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
